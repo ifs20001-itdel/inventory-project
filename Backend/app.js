b17506377e894e760e7f2303beb9b28523513688
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-11-14 10:57:50 +0700

    Connect To Frontend

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,10 @@ var logRouter = require('./routes/logs')
 
 var app = express();
 
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:3001'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
