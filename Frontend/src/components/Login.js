7bf99deca4421c24de3d35e8b7b4536000a3e60e
unknown <hadyansyaputra00@gmail.com>
2023-11-14 22:07:54 +0700

    Login page Only

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../style/login.css'
+import loginImg from '../assest/pexels-ehsan-ahmadnejad-1371985.jpg';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -25,44 +27,44 @@ const Login = () => {
         }
     };
 
+    const navigateToRegister = () => {
+        navigate("/register");
+    };
+
     return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
-                            <form onSubmit={handleLogin} className="box">
-                                {msg && <p className="has-text-centered has-text-danger">{msg}</p>}
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input
-                                            type="text"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            className="input"
-                                            placeholder="your email"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input
-                                            type="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            className="input"
-                                            placeholder="********"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Login</button>
-                                </div>
-                            </form>
+        <section>
+            <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+                <div className="hidden sm:block">
+                    <img className="w-full h-full object-cover" src={loginImg} alt=""></img>
+                </div>
+
+                <div className="bg-gray-800 flex flex-col justify-center">
+                    <form onSubmit={handleLogin} className="max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg">
+                        <h2 className="text-4xl dark:text-white font-bold text-center">SIGN IN</h2>
+                        {msg && <p>{msg}</p>}
+                        <div className="flex flex-col text-gray-400 py-2">
+                            <label>Email</label>
+                            <input
+                                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                                type="text"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="your email"
+                            />
                         </div>
-                    </div>
+                        <div className="flex flex-col text-gray-400 py-2">
+                            <label>Password</label>
+                            <input
+                                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="********"
+                            />
+                        </div>
+                        <button className="w-full my-2 py-2 bg-blue-500 shadow-lg shadow-blue-500/50 hover:shadow-blue-500/40 text-white font-semibold rounded-lg" onClick={navigateToRegister}>Go to Register</button>
+                        <button className="w-full my-2 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">Sign In</button>
+                    </form>
                 </div>
             </div>
         </section>
